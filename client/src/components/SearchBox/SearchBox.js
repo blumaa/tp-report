5a61be0afd3ad2a16f259f7acd5191c6350f9fe2
blumaa <blumaa@gmail.com>
2020-04-08 01:26:56 +0200

    search box ui update

--- client/src/components/SearchBox/SearchBox.js
+++ client/src/components/SearchBox/SearchBox.js
@@ -1,30 +1,44 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 const SearchBox = ({ changeLocation }) => {
   const [location, setLocation] = useState(null);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     setLocation(e.target.value);
   };
 
-  const handleSubmit = async e => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     changeLocation(location);
   };
 
   return (
-    <form onSubmit={handleSubmit} id="search-form">
-      <input
-        id="search-box"
-        type="text"
-        name="search"
-        placeholder="what is your location?"
-        onChange={handleChange}
-      />
-      <button id="search-button">Search</button>
-    </form>
+    <div id="main-message">
+      <div id="message-container">
+        <div id="main-message-left">
+          Report if a store is out of toilet paper!
+        </div>
+      </div>
+      <div id="search-box-container">
+        <form onSubmit={handleSubmit} id="search-form">
+          <input
+            id="search-box"
+            type="text"
+            name="search"
+            placeholder="what is your location?"
+            onChange={handleChange}
+          />
+          <button id="search-button">Search</button>
+        </form>
+      </div>
+      <div id="message-container">
+        <div id="main-message-right">
+          {" "}
+          Help social distancing! Help those on the front line!
+        </div>
+      </div>
+    </div>
   );
 };
 
